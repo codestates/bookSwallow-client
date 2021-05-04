7411f65daf9e8ff5bdca92672929e2ac089ffd1b
chotaegyu <chotg5592@gmail.com>
2021-05-04 10:15:58 +0900

    feat: comment 상태관리 개발중'

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,12 +3,14 @@ import books from './books';
 import auth from './auth';
 import user from './user';
 import zzims from './zzims';
+import comments from './comments';
 
 const rootReducer = combineReducers({
   books,
   auth,
   user,
   zzims,
+  comments,
 });
 
 export default rootReducer;
